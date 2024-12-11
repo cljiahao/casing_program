2bc7af4d38ecbe357cb993a71ffb5272ad62b490
CLJiaHao <clarence.lee@murata.com>
2024-12-11 12:54:36 +0800

    add constants file for font and wos labels

---
+++ src/core/constants.py
@@ -0,0 +1,71 @@
+font_size = {
+    "XS": ("Calibri", "12"),
+    "S": ("Calibri", "14"),
+    "M": ("Calibri", "16"),
+    "L": ("Calibri", "18"),
+    "XL": ("Calibri", "20"),
+    "2XL": ("Calibri", "22"),
+    "3XL": ("Calibri", "24"),
+}
+wos_labels = [
+    {
+        "name": "Machine No",
+        "key": "mcNo",
+        "value": "01",
+        "type": "label",
+    },
+    {
+        "name": "Operator ID",
+        "key": "OptCode",
+        "value": "",
+        "type": "label",
+    },
+    {
+        "name": "Lot No",
+        "key": "lotNo",
+        "value": "",
+        "type": "entry",
+    },
+    {
+        "name": "Container ID",
+        "key": "contid",
+        "value": "",
+        "type": "entry",
+    },
+    {
+        "name": "Reel Barcode ID",
+        "key": "Reelid",
+        "value": "",
+        "type": "entry",
+    },
+    {
+        "name": "Inspection No",
+        "key": "inspectionNo",
+        "value": "",
+        "type": "label",
+    },
+    {
+        "name": "No of Reels",
+        "key": "noOfReel",
+        "value": "",
+        "type": "label",
+    },
+    {
+        "name": "Item Type",
+        "key": "Item",
+        "value": "",
+        "type": "label",
+    },
+    {
+        "name": "Container Count",
+        "key": "noOfBox",
+        "value": "",
+        "type": "label",
+    },
+    {
+        "name": "Qty of Finished Goods",
+        "key": "chipQty",
+        "value": "",
+        "type": "label",
+    },
+]
